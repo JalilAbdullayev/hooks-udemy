28ed0e386fa17b9ad9b7015ccbaaaa85357309f6
JalilAbdullayev <abdullacelil00@gmail.com>
2024-06-08 10:23:09 +0400

    Application Design

---
+++ src/components/ToDoList.jsx
@@ -0,0 +1,19 @@
+const ToDoList = () => {
+    return (
+        <>
+            <ul>
+                <li>
+                    Pay bills
+                </li>
+                <li>
+                    Do your homework
+                </li>
+                <li>
+                    Feed the dog
+                </li>
+            </ul>
+        </>
+    );
+}
+
+export default ToDoList;
