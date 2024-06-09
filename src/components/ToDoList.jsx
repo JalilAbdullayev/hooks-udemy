07a47ecf01101888dc57bf9fc239181b791c472e
JalilAbdullayev <abdullacelil00@gmail.com>
2024-06-09 07:27:32 +0400

    useState Hook

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,17 +1,41 @@
+import { useState } from "react";
+
 const ToDoList = () => {
+    const [todos, setTodos] = useState([{
+        id: 1,
+        text: "Pay bills"
+    }, {
+        id: 2,
+        text: "Do your homework"
+    }, {
+        id: 3,
+        text: "Feed the dog"
+    }]);
+
+    const addToDo = () => {
+        setTodos([
+            ...todos,
+            {
+                id: Math.random(),
+                text: "Learn Hooks"
+            }
+        ]);
+    }
+
     return (
         <>
             <ul>
-                <li>
-                    Pay bills
-                </li>
-                <li>
-                    Do your homework
-                </li>
-                <li>
-                    Feed the dog
-                </li>
+                {todos.map(todo => {
+                    return (
+                        <li key={todo.id}>
+                            {todo.text}
+                        </li>
+                    )
+                })}
             </ul>
+            <button onClick={addToDo}>
+                Add A To Do
+            </button>
         </>
     );
 }
